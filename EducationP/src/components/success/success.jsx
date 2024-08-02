2732f390637f36e20ded2128de9c9f3b073dfd84
Manish Gyawali <104677087+manishgyawali@users.noreply.github.com>
2024-08-02 18:21:56 +0545

    Delete EducationP directory

--- EducationP/src/components/success/success.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Success = () => {
-  return (
-    <div className='bg-red-400'>success</div>
-  )
-}
-
-export default Success
--- EducationP/src/components/toolbar/Routes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Routes = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default Routes
